a13707ac6e4e85a998adbeb6b181760d3ab8fa6e
udday deol <uddaydeol294@gmail.com>
2025-06-17 23:25:33 +0530

    Update: made small changes

--- lib/utils.ts
+++ lib/utils.ts
@@ -54,8 +54,6 @@ export const configureAssistant = (voice: string, style: string) => {
         },
       ],
     },
-    clientMessages: [],
-    serverMessages: [],
   };
   return vapiAssistant;
 };
